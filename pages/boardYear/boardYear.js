44aff25d89bb6f34517cf44580d8c8b3235ef5dd
xiaoxiao <253451518@qq.com>
2019-02-21 19:31:28 +0800

    年度榜单列表详情页

--- pages/boardYear/boardYear.js
+++ pages/boardYear/boardYear.js
@@ -5,103 +5,43 @@ const localData = require('../../data/data.js')
 
 Page({
   data: {
-    subTitle: '',
-    title: '',
-    year: '',
-    index: '',
-    value: '',//请求数据的参数
+    year:'2018',
     hasData: false,//是否有数据
     yearData: [],//年度电影榜单
-    praiseWeek: [],//一周口碑电影榜
-    topData: [],//top250电影
-    year: '',
-    allCount:10,
-    type:0,//类型 0=》一周口碑电影榜 1=》年度排行榜 2=》top250
-
   },
   onLoad: function (options) {
-    console.log(options)
-    let year = (new Date()).getFullYear() - 1;
-    wx.setNavigationBarTitle({title:options.title});
-    this.setData({ title: options.title });
-    this.setData({ year: options.year });
-    this.setData({ index: options.index });
-    this.setData({ value: options.value });
-    if (options.value=='week') {
-      this.setData({type:0});
-      this.getWeekData();
-    }else if (options.value=='top250') {
-      this.setData({type:2});
-      this.getTopData();
-      this.setData({allCount:250});
-    }else{
-      this.setData({type:1});
-      this.getYearData(options.value);
-    }
+    //播放音乐
+    this.audioCtx = wx.createAudioContext('myAudio');
+    this.audioCtx.play();
+    //获取数据
+    this.getYearData('华语电影', 1, 0);
+    this.getYearData('外语电影', 2, 1);
+    this.getYearData('冷门佳片', 9, 2);
+    this.getYearData('科幻片', 20, 3);
   },
-  //获取一周口碑电影榜
-  getWeekData: function () {
-    var then = this;
-    const path = 'j/search_subjects';
-    let params = {
-      type: 'movie',
-      tag: '热门',
-      sort: 'recommend',
-      page_limit: '10',
-      page_start: '0'
-    };
-    app.douban(app.data.api, path, params)
-      .then(res => {
-          //获取评星数量
-          let dataArr = res.data.subjects;
-          for (let i = 0; i < dataArr.length; i++) {
-            dataArr[i].count = Math.round(dataArr[i].rate / 2);
-          }
-        then.setData({ praiseWeek: dataArr });
-      })
-      .catch(err => {
-        console.log(err)
-      })
-  },
-  //获取top250
-  getTopData: function () {
-    var then = this;
-    const itemApi = "https://api.douban.com";
-    const path = 'v2/movie/top250';
-    app.douban(itemApi, path, {})
-      .then(res => {
-        if (res.statusCode != 200) {
-          res = localData.top250;
-        }
-        //获取评星数量
-        let dataArr = res.subjects;
-        for (let i = 0; i < dataArr.length; i++) {
-          dataArr[i].count = Math.round(dataArr[i].rating.average / 2);
-        }
-        res.subjects=dataArr;
-        then.setData({ topData: res.subjects });
-      })
-      .catch(err => {
-        console.log(err)
-      })
-  },
-  //封装函数 获取年度各类型电影排行榜 
+ //封装函数 获取年度各类型电影排行榜 
   //value表示的是排行榜类型 1=》华语 2=》外语 9=》冷门佳片 20=》科幻片
-  getYearData: function (value) {
+  getYearData: function (title, value, index) {
     let year = (new Date()).getFullYear() - 1;
+    this.setData({year,year});
     let itemApi = 'https://movie.douban.com';
     let then = this;
+    console.log(year)
     const path = 'ithil_j/activity/movie_annual' + year + '/widget/' + value;
     app.douban(itemApi, path, {})
       .then(res => {
+        console.log(res)
         if (res.data.r == 0) {
-            //获取评星数量
-        let dataArr = res.data.res.subjects;
-        for (let i = 0; i < dataArr.length; i++) {
-          dataArr[i].count = Math.round(dataArr[i].rating / 2);
-        }
+          let itemObj = {
+            title: title,
+            data: res.data.res,
+            value:value
+          }
+          let yearData = then.data.yearData;
+          //每次请求数据后 请数据添加到yearData中
+          yearData[index] = itemObj;
           then.setData({ hasData: true });
-          then.setData({ yearData: dataArr });
+          then.setData({ yearData: yearData });
         }
       })
       .catch(err => {
