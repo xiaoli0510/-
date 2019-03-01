0336614fa4ceb5ded8284da273e4af3a7dae34dd
xiaoxiao <253451518@qq.com>
2019-03-01 18:58:17 +0800

    登录

--- pages/login/login.js
+++ pages/login/login.js
@@ -20,5 +20,24 @@ Page({
     }],
   },
   onLoad: function () {
+  },
+  //微信登录
+  wechatLogin:function(){
+    wx.showModal({
+      title: '提示',
+      content: '这是一个模态弹窗',
+      success(res) {
+        if (res.confirm) {
+          console.log('用户点击确定')
+        } else if (res.cancel) {
+          console.log('用户点击取消')
+        }
+      }
+    })
+
+  },
+  //账号登录
+  accountLogin:function(){
+
   }
 })
